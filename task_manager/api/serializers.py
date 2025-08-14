ac43bcbff565b9c8715234efbc48515e1b1d245a
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-14 18:48:09 +0300

    refactor: update serializers to use consistent import style and include password field in UserSerializer

--- task_manager/api/serializers.py
+++ task_manager/api/serializers.py
@@ -1,14 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Task
 from django.contrib.auth.models import User
 
-class TaskSerializer(ModelSerializer):
+
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
 
-class UserSerializer(ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=6)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
