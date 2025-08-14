0ed76300cd23052d6843a30f77607da5005c3343
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-14 19:03:06 +0300

    feat: add profile update and delete endpoints to the API

--- task_manager/api/urls.py
+++ task_manager/api/urls.py
@@ -3,9 +3,11 @@
 from django.urls import path
 
 
-from .views import LoginView, ProfileView, RegisterView
+from .views import DeleteProfileView, LoginView, ProfileView, RegisterView, UpdateProfileView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),    
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/update/', UpdateProfileView.as_view(), name='update_profile'),
+    path('profile/delete/', DeleteProfileView.as_view(), name='delete_profile'),    
 ]
