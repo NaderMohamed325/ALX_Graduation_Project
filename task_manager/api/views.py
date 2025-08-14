64209de4e0e226be98deadd1fc0a1daadda48b4d
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-14 18:19:54 +0300

    feat: implement user registration view with serializer validation

--- task_manager/api/views.py
+++ task_manager/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"user": UserSerializer(user).data}, status=201)
+        return Response(serializer.errors, status=400)
