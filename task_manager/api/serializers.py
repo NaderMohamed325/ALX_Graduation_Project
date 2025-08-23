5a7680c94a0747d724269c1b2eb2dd2b030916b4
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-23 14:28:51 +0300

    feat: update TaskSerializer fields and enhance UserSerializer with first_name and last_name

--- task_manager/api/serializers.py
+++ task_manager/api/serializers.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'due_date', 'completed', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'completed', 'created_at', 'updated_at']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -23,3 +24,12 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
