d3b0c16450c887624b24d40351a0758c0ebbf156
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-23 14:28:47 +0300

    feat: update Task model to allow blank description and add related_name for user field

--- task_manager/api/models.py
+++ task_manager/api/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     due_date = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
